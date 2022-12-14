30d19d70d01080358cfbcdf1b4b8bb9b4b9070b1
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-12-14 16:06:26 +0200

    Start

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.css';
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={styles.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id} className={styles.item}>
+        <p className={styles.contact}>
+          {name}:{number}
+        </p>
+        <button
+          className={styles.btn}
+          type="submit"
+          onClick={() => onDeleteContact(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  onDeleteContact: PropTypes.func,
+};
+
+export default ContactList;
